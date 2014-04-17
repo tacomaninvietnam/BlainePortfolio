2bc2bdc200805bb493360650a1d6b53bae87c9ca
tacomaninvietnam <twostrippersandacrackpipe@yahoo.com>
2014-04-17 16:48:51 -0400

    Added New Project Folder For Independent Study
    Added Numerous Examples of Basic Operations Using Multithreading,
    including how to create threads, tasks, use of atomic interlocked class
    operations, and basic thread control.  Also included are Examples of ho
    to cancel threads as well as as speed performance comparison.  This
    Should Satisfy at least one badge of my independent studies.

---
+++ Independant Research Project/CreatingParameterizedThread/ParameterizedThreadStarting/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Blaine Simcox 
+ * Example of Starting Threads With Parameters
+ * CIT 275
+ * Jeff Straw
+ * Example using code supplied via Youtube video by Jesse Diedrichson 40-783 1.1 - 1.2 tutorials
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParameterizedThreadStarting
+{
+    class Program
+    {
+        public static void ThreadMethod(object o)   //Method for ParameterizedThreadStart delegate.  Requires type of object
+        {
+            for ( int i = 0; i < (int)o; i++)       //Cast object o int an int for comparison
+            {
+                Console.WriteLine("ThreadPrc: {0}", i);  //Output i ever 1/2 second
+                Thread.Sleep(500);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is an example of a Parameterized Thread Call.\nThis Thread has been initialized t cycle 15 \ntimes at 1/2 second intervals before stopping.");
+            Thread t = new Thread(new ParameterizedThreadStart(ThreadMethod));   //Create new parameterized Thread
+            t.Start(15);                               //start thread with parameter of 15 cycles for ThreadMethod
+            t.Join();                          //join stops application from terminating before thread is complete
+            Console.WriteLine("Press Enter to Continue!");
+            Console.ReadKey();                 //stop so user can read output       
+        }
+    }
+}
---
+++ Independant Research Project/StoppingThreads/StoppingThreads/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Blaine Simcox 
+ * Example of Speed Difference when multithreading tasks
+ * CIT 275
+ * Jeff Straw
+ * Example using code From 70-483 videos from DredricOne on youtube.com
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace StoppingThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool stopped = false;
+            Thread t = new Thread(new ThreadStart(() =>
+                {
+                    while (!stopped)
+                    {
+                        Console.WriteLine("Running.....");
+                        Thread.Sleep(1000);
+
+                    }
+                    Console.WriteLine("Thread is About to close.");
+                }));
+            t.Start();
+            Console.WriteLine("Press Any Key to Exit.");
+            Console.ReadKey();
+            stopped = true;
+            t.Join();
+        }
+    }
+}
