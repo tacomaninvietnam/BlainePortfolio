e9121afcd616b3362a7d372ec06cf9be94a37eff
tacomaninvietnam <twostrippersandacrackpipe@yahoo.com>
2014-04-19 03:51:57 -0400

    Put Experiment base in Folder
    Put unfinished ExperimentBase project in its own folder to organize unit
    testing folder.

---
+++ SimcoxB_UnitTestingExample/ExperimentBase Project/ExperimentBase/Foo.cs
@@ -0,0 +1,116 @@
+﻿/*******************************************************************************
+ * Generic custom class to investigate Unit Tests in Visual Studio
+ * Jeff Straw | Northwestern Michigan College
+ * 01/26/2014: Initial
+ ******************************************************************************/
+using System;
+using System.Globalization;
+
+namespace ExperimentBase
+{
+    /// <summary>
+    /// Generic multi-property custom object
+    /// </summary>
+    
+    public class Foo
+    {
+        #region [ Business rules and local configuration ]
+        private TextInfo usTextInfo = new CultureInfo("en-US", false).TextInfo;   // Select among global options
+
+        private const string DefaultName = "";
+        private const int DefaultRating = 1;
+        private const bool DefaultRegistered = false;
+        private readonly static DateTime DefaultWhenRegistered = DateTime.Today;
+        #endregion
+
+        #region [ Fields ]
+        private string _name;
+        private int  _rating;
+        private DateTime _whenPurchased;
+        #endregion
+
+        #region [ Properties ]
+        /// <summary>
+        /// Common name of this Foo using U.S. English title case
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value == null ? null : usTextInfo.ToTitleCase(value.Trim()); }
+        }
+
+        /// <summary>
+        /// Number of stars - 1 through 5
+        /// </summary>
+        public int Rating
+        {
+            get { return _rating; }
+            set { _rating = value; }
+        }
+
+        /// <summary>
+        /// Whether or not this Foo has been licensed and registered with the manufacturer
+        /// </summary>
+        public bool Registered { get; set; }
+
+        /// <summary>
+        /// Date when current owner purchased this Foo
+        /// </summary>
+        public DateTime WhenPurchased
+        {
+            get { return _whenPurchased; }
+            set { _whenPurchased = value.Date; }    // Strip time of day detail
+        }
+        #endregion
+
+        #region [ Constructors ]
+        /// <summary>
+        /// Generic multi-property custom object
+        /// </summary>
+        public Foo()
+            : this(DefaultName, DefaultRating, DefaultRegistered, DefaultWhenRegistered)
+        {
+        }
+
+        /// <summary>
+        /// Generic multi-property custom object
+        /// </summary>
+        /// <param name="name">Common name of this Foo</param>
+        /// <param name="rating">Number of stars - 1 through 5</param>
+        public Foo(string name, int rating)
+            : this(name, rating, DefaultRegistered, DefaultWhenRegistered)
+        {
+        }
+
+        /// <summary>
+        /// Generic multi-property custom object
+        /// </summary>
+        /// <param name="name">Common name of this Foo</param>
+        /// <param name="rating">Number of stars - 1 through 5</param>
+        /// <param name="registered">Whether or not this Foo has been licensed and registered with the manufacturer</param>
+        /// <param name="whenPurchased">Date when current owner purchased this Foo</param>
+        public Foo(string name, int rating, bool registered, DateTime whenPurchased)
+        {
+            this.Name = name;
+            this.Rating = rating;
+            this.Registered = registered;
+            this.WhenPurchased = whenPurchased;
+        } 
+        #endregion
+
+        #region [ Methods ]
+        /// <summary>
+        /// Custom text representation of a Foo object
+        /// </summary>
+        /// <returns>String representation of this Foo</returns>
+        public override string ToString()
+        {
+            return string.Format("{0} purchased {1:MM/dd/yyyy} {2} ({3})",
+                this.Name,
+                this.WhenPurchased,
+                this.Registered ? "is registered" : "is not registered",
+                new string('*', this.Rating));
+        } 
+        #endregion
+    }
+}
---
+++ SimcoxB_UnitTestingExample/ExperimentBase Project/SimcoxBUnitTesting/ExperimentBaseTest.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExperimentBase;
+
+namespace SimcoxBUnitTesting
+{
+    [TestClass]
+    public class ExperimentBaseTest
+    {
+        #region NameTests
+        [TestMethod]
+        public void NameTest()
+        {
+            Foo foo = new Foo();
+            foo.Name = "Straego";
+            string actual = foo.Name;
+
+            string expected = "Straego";
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void HandleNull()
+        {
+            Foo foo = new Foo();
+            try
+            {
+                foo.Name = null;
+            }
+            catch (ArgumentNullException)
+            {
+
+            }
+        }
+
+        [TestMethod]
+        public void NullNameTest()
+        {
+            Foo foo = new Foo();
+            foo.Name = null;
+
+            Assert.IsNull(foo.Name);
+        }
+
+        [TestMethod]
+        public void NameIsEmpty()
+        {
+            Foo foo = new Foo();
+            Assert.AreEqual(foo.Name, "");
+        } 
+        #endregion
+
+        #region RatingTests
+        [TestMethod]
+        public void RatingNotEqual()
+        {
+            Foo foo = new Foo();
+            foo.Rating = 12;
+
+            Assert.AreNotEqual(foo.Rating, 14);
+        }
+
+        [TestMethod]
+        public void RatingTooHigh()
+        {
+            Foo foo = new Foo();
+            int testRating = 2 ^ 3;
+
+            foo.Rating = 6;
+            Assert.AreNotEqual(foo.Rating, testRating);
+
+        }
+
+        [TestMethod]
+        public void RatingNotNull()
+        {
+            Foo foo = new Foo();
+            Assert.IsNotNull(foo.Rating);
+
+        }
+
+        [TestMethod]
+        public void RatingNumberInput()
+        {
+            Foo foo = new Foo();
+            foo.Rating = 098765432;
+            int actual = 098765432;
+            Assert.AreEqual(foo.Rating, actual);
+
+        } 
+        #endregion
+
+        #region RegisteredTests
+        [TestMethod]
+        public void IsRegisteredTest()
+        {
+            Foo foo = new Foo();
+            foo.Registered = true;
+
+            Assert.IsTrue(foo.Registered);
+        }
+
+        [TestMethod]
+        public void NotRegisteredTest()
+        {
+            Foo foo = new Foo();
+            foo.Registered = false;
+
+            Assert.IsFalse(foo.Registered);
+        }
+
+        [TestMethod]
+        public void RegisteredDefaultFalseTest()
+        {
+            Foo foo = new Foo();
+
+            Assert.IsFalse(foo.Registered);
+        } 
+        #endregion
+
+        #region WhenPurchasedTests
+        [TestMethod]
+        public void WhenPurchasedEqulityTest()
+        {
+            Foo foo = new Foo();
+            DateTime time = new DateTime();
+
+            foo.WhenPurchased = time;
+
+            Assert.AreEqual(time, foo.WhenPurchased);
+        }
+
+        [TestMethod]
+        public void WhenPurchasedDataEntryTest()
+        {
+            Foo foo = new Foo();
+            DateTime time = new DateTime(2013, 1, 10);
+
+            foo.WhenPurchased = new DateTime(2013, 1, 10);
+
+            Assert.AreEqual(time, foo.WhenPurchased);
+        }
+
+        [TestMethod]
+        public void WhenPurchasedOutOfRangeTest()
+        {
+            Foo foo = new Foo();
+
+            try
+            {
+                foo.WhenPurchased = new DateTime(1, 10, 2013);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+
+            }
+
+        } 
+        #endregion
+
+        [TestMethod]
+        public void FooClassEqualityTest()
+        {
+            DateTime time = new DateTime(2013,1, 10);
+            Foo foo = new Foo("bob", 16, true, time);
+            Foo dog = new Foo("bob", 16, true, time);
+
+            Assert.AreEqual(foo.Name, dog.Name);
+            Assert.AreEqual(foo.Registered, dog.Registered);
+            Assert.AreEqual(foo.Rating, dog.Rating);
+            Assert.AreEqual(foo.WhenPurchased, dog.WhenPurchased);
+            
+        }
+
+        [TestMethod]
+        public void FooClassInequalityTest()
+        {
+            DateTime time = new DateTime(2013, 1, 10);
+            DateTime time2 = new DateTime(1999, 1, 10);
+
+            Foo foo = new Foo("bob", 16, true, time);
+            Foo dog = new Foo("George", 23, false, time2);
+
+            Assert.AreNotEqual(foo.Name, dog.Name);
+            Assert.AreNotEqual(foo.Registered, dog.Registered);
+            Assert.AreNotEqual(foo.Rating, dog.Rating);
+            Assert.AreNotEqual(foo.WhenPurchased, dog.WhenPurchased);
+
+        }
+
+        [TestMethod]
+        public void FooClassConstructorAndTitleCaseEqualityTest()
+        {
+            DateTime time = new DateTime(2013, 1, 10);
+            DateTime time2 = new DateTime(2013, 1, 10);
+            
+            Foo foo = new Foo("bob", 16, true, time);
+
+            Assert.AreEqual(foo.Name, "Bob");
+            Assert.AreEqual(foo.Registered, true);
+            Assert.AreEqual(foo.Rating, 16);
+            Assert.AreEqual(foo.WhenPurchased, time2);
+
+        }
+        
+        [TestMethod]
+        public void ToStringTest()
+        {
+            DateTime time = new DateTime(2013, 1, 10);
+            Foo foo = new Foo("bob", 16, true, time);
+            string test = foo.ToString(); 
+
+            Assert.AreEqual(foo.ToString(), test);
+        }
+    }
+
+
+}
